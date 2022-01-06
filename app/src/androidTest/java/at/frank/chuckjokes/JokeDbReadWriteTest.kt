020dea72aab47a07d48e42e677b4645d5dd1478a
c.frank <c.frank@sportradar.com>
2022-01-06 14:22:13 +0100

    Adds testsuite for testing database.

---
+++ app/src/androidTest/java/at/frank/chuckjokes/JokeDbReadWriteTest.kt
@@ -0,0 +1,49 @@
+package at.frank.chuckjokes
+
+import android.content.Context
+import androidx.room.Room
+import androidx.test.core.app.ApplicationProvider
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import at.frank.chuckjokes.data.local.JokeDBEntity
+import at.frank.chuckjokes.data.local.JokeDao
+import at.frank.chuckjokes.data.local.JokeDatabase
+import org.junit.After
+import org.junit.Before
+import org.junit.Test
+import org.junit.runner.RunWith
+import java.io.IOException
+
+/**
+ * Instrumented test, which will execute on an Android device.
+ *
+ * See [testing documentation](http://d.android.com/tools/testing).
+ */
+@RunWith(AndroidJUnit4::class)
+class ExampleInstrumentedTest {
+    private lateinit var userDao: JokeDao
+    private lateinit var db: JokeDatabase
+
+    @Before
+    fun createDb() {
+        val context = ApplicationProvider.getApplicationContext<Context>()
+        db = Room.inMemoryDatabaseBuilder(
+            context, JokeDatabase::class.java
+        ).build()
+        userDao = db.jokeDao()
+    }
+
+    @After
+    @Throws(IOException::class)
+    fun closeDb() {
+        db.close()
+    }
+
+    @Test
+    @Throws(Exception::class)
+    fun writeUserAndReadInList() {
+        val joke = JokeDBEntity("1234", "", "funny joke", "")
+        userDao.bookmarkJoke(joke).test()
+        userDao.getBookmarkedJokes().test().assertComplete().assertValue { it.contains(joke) }
+            .dispose()
+    }
+}
