71c9c451b545335dac5839a04d8a56540a91885c
c.frank <c.frank@sportradar.com>
2022-01-09 19:26:23 +0100

    moves MainActivity.kt, styles and themes to presentation layer.

--- presentation/src/main/java/at/frank/presentation/MainActivity.kt
+++ presentation/src/main/java/at/frank/presentation/MainActivity.kt
@@ -1,8 +1,7 @@
-package at.frank.chuckjokes.ui
+package at.frank.presentation
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import at.frank.chuckjokes.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
