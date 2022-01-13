04879d0bac577b9034a67b116cd197a8008df7c6
c.frank <c.frank@sportradar.com>
2022-01-13 10:13:26 +0100

    code cleanup

--- app/src/main/java/at/frank/chuckjokes/JokeApp.kt
+++ app/src/main/java/at/frank/chuckjokes/JokeApp.kt
@@ -2,26 +2,13 @@ package at.frank.chuckjokes
 
 import android.app.Application
 import androidx.appcompat.app.AppCompatDelegate
-import androidx.room.Room
-import at.frank.chuckjokes.data.local.JokeDatabase
-import at.frank.chuckjokes.data.remote.ChuckNorrisApi
-import at.frank.chuckjokes.data.remote.RetroFitModule
 import at.frank.chuckjokes.di.ApplicationComponent
-import at.frank.chuckjokes.di.ApplicationModule
 import at.frank.chuckjokes.di.DaggerApplicationComponent
 import at.frank.chuckjokes.di.DatabaseModule
-import at.frank.chuckjokes.domain.*
-import at.frank.chuckjokes.presentation.JokeAppContract
-import io.reactivex.Scheduler
-import io.reactivex.android.schedulers.AndroidSchedulers
-import io.reactivex.schedulers.Schedulers
-
 
 class JokeApp : Application() {
 
     lateinit var applicationComponent: ApplicationComponent
-
-
 
     override fun onCreate() {
         AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_FOLLOW_SYSTEM)
@@ -33,5 +20,4 @@ class JokeApp : Application() {
 
         super.onCreate()
     }
-
 }
--- app/src/main/java/at/frank/chuckjokes/di/ApplicationModule.kt
+++ app/src/main/java/at/frank/chuckjokes/di/ApplicationModule.kt
@@ -1,15 +1,13 @@
 package at.frank.chuckjokes.di
 
-import android.app.Application
 import at.frank.chuckjokes.RxSchedulers
 import dagger.Module
 import dagger.Provides
 import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.schedulers.Schedulers
-import javax.inject.Singleton
 
 @Module
-class ApplicationModule() {
+class ApplicationModule {
 
     @Provides
     fun provideRxSchedulers(): RxSchedulers =
